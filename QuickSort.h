547b73ecc0638ed821f3630bdfe42956a42c1061
git-meteor <mailtohuangxin@gmail.com>
2015-04-12 16:41:21 +0800

    quick sort 修改为policy模式

--- QuickSort.h
+++ QuickSort.h
@@ -1,26 +1,18 @@
 #include "Common.h"
 #include <assert.h>
 
-class QuickSort : public TestCase {
+template<typename T> class MedianSelector {
 public:
-
-	void Sort(int num[], int left, int right){
-		if(left < right){
-			int pivot_index = SelectPivot(num, left, right);
-			std::swap(num[pivot_index], num[right]);
-			int partition_index = Partition(num, left, right - 1, num[right]);
-			std::swap(num[partition_index], num[right]);
-			Sort(num, left, partition_index - 1);
-			Sort(num, partition_index + 1, right);
-		}
+	MedianSelector() {;}
+	int operator()(T num[], int left, int right){
+		return (left+right) >> 1;
 	}
+};
 
-
-	int SelectPivot(int num[], int left, int right){
-		return (left + right) >> 1;
-	}
-
-	int Partition(int num[], int left, int right, int pivot_value){
+template<typename T> class NormalPartitioner {
+public:
+	NormalPartitioner() {;}
+	int operator()(T num[], int left, int right, T pivot_value){
 		int i = left;
 		int j = right;
 		while(i <= j){
@@ -43,9 +35,60 @@ public:
 
 		return i;
 	}
+};
 
-	void Run(int num[], int n) {
+template<typename T, class PivotSelector, class Partitioner> class QuickSort {
+	PivotSelector pivot_selector_;
+	Partitioner partitioner_;
+
+public:
+	QuickSort() {;}
+
+	void Sort(T num[], int left, int right){
+		if(left < right){
+			int pivot_index = pivot_selector_(num, left, right);
+
+			std::swap(num[pivot_index], num[right]);
+			int partition_index = partitioner_(num, left, right - 1, num[right]);
+
+			std::swap(num[partition_index], num[right]);
+			Sort(num, left, partition_index - 1);
+			Sort(num, partition_index + 1, right);
+		}
+	}
+
+	void Run(T num[], int n) {
 		Sort(num, 0, n - 1);
+	}
+};
+
+class QuickSortTest : public TestCase  {
+	virtual void Test(){
+		QuickSort<int, MedianSelector<int>, NormalPartitioner<int> > sorter;
+//		QuickSort<int> sorter;
+
+		srand(12345);
+		int test_count = 10;
+		int max_array_size = 10;
+		for(int i = 0; i < test_count; ++i){
+			for(int j = 1; j <= max_array_size; ++j){
+				int array_size = j;
+				int* a = new int[array_size];
+
+				for(int k = 0; k < array_size; ++k){
+					a[k] = rand() % array_size;
+				}
+
+				std::cout << "Before sort: ";
+				Print(a, array_size);
+				sorter.Run(a, array_size);
+				std::cout << "After sort: ";
+				Print(a, array_size);
+				assert(IsSorted(a, array_size));
+
+				delete a;
+			}
+		}
 	}
 
 	bool IsSorted(int num[], int n){
@@ -67,31 +110,6 @@ public:
 			std::cout << num[i] << " ";
 		}
 		std::cout << std::endl;
-	}
-
-	virtual void Test(){
-		srand(12345);
-		int test_count = 10;
-		int max_array_size = 10;
-		for(int i = 0; i < test_count; ++i){
-			for(int j = 1; j <= max_array_size; ++j){
-				int array_size = j;
-				int* a = new int[array_size];
-
-				for(int k = 0; k < array_size; ++k){
-					a[k] = rand() % array_size;
-				}
-
-				std::cout << "Before sort: ";
-				Print(a, array_size);
-				Run(a, array_size);
-				std::cout << "After sort: ";
-				Print(a, array_size);
-				assert(IsSorted(a, array_size));
-
-				delete a;
-			}
-		}
 	}
 };
 
--- main.cc
+++ main.cc
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]){
 
 	TestCase* test_case_array[] = {
 //			new BinarySearch,
-			new QuickSort,
+			new QuickSortTest,
 	};
 
 
