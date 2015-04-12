fe2f24efc40a964dbb96f92afb3f09952a01e2fa
git-meteor <mailtohuangxin@gmail.com>
2015-04-12 17:21:43 +0800

    将Selector和Partitioner移动到Common.h

--- Common.h
+++ Common.h
@@ -3,6 +3,42 @@
 
 #define ARRAY_SIZE(a) (sizeof((a))/sizeof(int))
 
+template<typename T> class MedianSelector {
+public:
+	MedianSelector() {;}
+	int operator()(T num[], int left, int right){
+		return (left+right) >> 1;
+	}
+};
+
+template<typename T> class NormalPartitioner {
+public:
+	NormalPartitioner() {;}
+	int operator()(T num[], int left, int right, T pivot_value){
+		int i = left;
+		int j = right;
+		while(i <= j){
+			while(i <= right && num[i] < pivot_value){
+				++i;
+			}
+
+			while(j >= left && num[j] > pivot_value){
+				--j;
+			}
+
+			if(j <= i){
+				return i;
+			}
+
+			std::swap(num[i], num[j]);
+			++i;
+			--j;
+		}
+
+		return i;
+	}
+};
+
 class TestCase {
 public:
 	virtual void Test() = 0;
--- QuickSort.h
+++ QuickSort.h
@@ -1,42 +1,6 @@
 #include "Common.h"
 #include <assert.h>
 
-template<typename T> class MedianSelector {
-public:
-	MedianSelector() {;}
-	int operator()(T num[], int left, int right){
-		return (left+right) >> 1;
-	}
-};
-
-template<typename T> class NormalPartitioner {
-public:
-	NormalPartitioner() {;}
-	int operator()(T num[], int left, int right, T pivot_value){
-		int i = left;
-		int j = right;
-		while(i <= j){
-			while(i <= right && num[i] < pivot_value){
-				++i;
-			}
-
-			while(j >= left && num[j] > pivot_value){
-				--j;
-			}
-
-			if(j <= i){
-				return i;
-			}
-
-			std::swap(num[i], num[j]);
-			++i;
-			--j;
-		}
-
-		return i;
-	}
-};
-
 template<typename T, class PivotSelector, class Partitioner> class QuickSort {
 	PivotSelector pivot_selector_;
 	Partitioner partitioner_;
