435b70e2f33a6fde6a2b649762ce4ef25311f817
git-meteor <mailtohuangxin@gmail.com>
2015-04-13 09:09:15 +0800

    删除注释

--- BinarySearch.h
+++ BinarySearch.h
@@ -1,7 +1,7 @@
 #include "Common.h"
 #include <assert.h>
 
-class BinarySearch: public TestCase {
+class BinarySearch{
 public:
   int Run(int num[], int n, int v) {
     if (!num || n <= 0) {
@@ -23,95 +23,102 @@ public:
 
     return -1;
   }
+};
+
+class BinarySearchTest : public TestCase {
+public:
 
   virtual void Test() {
+
+    BinarySearch search;
+
     {
       int *a = 0;
-      assert(Run(a, 10, 5) == -1);
+      assert(search.Run(a, 10, 5) == -1);
     }
 
     {
       int a[1] = { 1 };
-      assert(Run(a, 0, 0) == -1);
-      assert(Run(a, 0, 1) == -1);
-      assert(Run(a, 0, 2) == -1);
+      assert(search.Run(a, 0, 0) == -1);
+      assert(search.Run(a, 0, 1) == -1);
+      assert(search.Run(a, 0, 2) == -1);
 
-      assert(Run(a, -1, 0) == -1);
-      assert(Run(a, -1, 1) == -1);
-      assert(Run(a, -1, 2) == -1);
+      assert(search.Run(a, -1, 0) == -1);
+      assert(search.Run(a, -1, 1) == -1);
+      assert(search.Run(a, -1, 2) == -1);
     }
 
     {
       int a[1] = { 1 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == -1);
     }
 
     {
       int a[2] = { 1, 3 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 3) == 1);
-      assert(Run(a, ARRAY_SIZE(a), 4) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 3) == 1);
+      assert(search.Run(a, ARRAY_SIZE(a), 4) == -1);
     }
 
     {
       int a[3] = { 1, 3, 5 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 3) == 1);
-      assert(Run(a, ARRAY_SIZE(a), 4) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 5) == 2);
-      assert(Run(a, ARRAY_SIZE(a), 6) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 3) == 1);
+      assert(search.Run(a, ARRAY_SIZE(a), 4) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 5) == 2);
+      assert(search.Run(a, ARRAY_SIZE(a), 6) == -1);
     }
 
     {
       int a[4] = { 1, 3, 5, 7 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 3) == 1);
-      assert(Run(a, ARRAY_SIZE(a), 4) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 5) == 2);
-      assert(Run(a, ARRAY_SIZE(a), 6) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 7) == 3);
-      assert(Run(a, ARRAY_SIZE(a), 8) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 3) == 1);
+      assert(search.Run(a, ARRAY_SIZE(a), 4) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 5) == 2);
+      assert(search.Run(a, ARRAY_SIZE(a), 6) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 7) == 3);
+      assert(search.Run(a, ARRAY_SIZE(a), 8) == -1);
     }
 
     {
       int a[5] = { 1, 3, 5, 7, 9 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 3) == 1);
-      assert(Run(a, ARRAY_SIZE(a), 4) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 5) == 2);
-      assert(Run(a, ARRAY_SIZE(a), 6) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 7) == 3);
-      assert(Run(a, ARRAY_SIZE(a), 8) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 9) == 4);
-      assert(Run(a, ARRAY_SIZE(a), 10) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 3) == 1);
+      assert(search.Run(a, ARRAY_SIZE(a), 4) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 5) == 2);
+      assert(search.Run(a, ARRAY_SIZE(a), 6) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 7) == 3);
+      assert(search.Run(a, ARRAY_SIZE(a), 8) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 9) == 4);
+      assert(search.Run(a, ARRAY_SIZE(a), 10) == -1);
     }
 
     {
       int a[9] = { 1, 2, 3, 5, 6, 7, 9, 10, 12 };
-      assert(Run(a, ARRAY_SIZE(a), 0) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 1) == 0);
-      assert(Run(a, ARRAY_SIZE(a), 2) == 1);
-      assert(Run(a, ARRAY_SIZE(a), 3) == 2);
-      assert(Run(a, ARRAY_SIZE(a), 4) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 5) == 3);
-      assert(Run(a, ARRAY_SIZE(a), 6) == 4);
-      assert(Run(a, ARRAY_SIZE(a), 7) == 5);
-      assert(Run(a, ARRAY_SIZE(a), 8) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 9) == 6);
-      assert(Run(a, ARRAY_SIZE(a), 10) == 7);
-      assert(Run(a, ARRAY_SIZE(a), 11) == -1);
-      assert(Run(a, ARRAY_SIZE(a), 12) == 8);
-      assert(Run(a, ARRAY_SIZE(a), 13) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 0) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 1) == 0);
+      assert(search.Run(a, ARRAY_SIZE(a), 2) == 1);
+      assert(search.Run(a, ARRAY_SIZE(a), 3) == 2);
+      assert(search.Run(a, ARRAY_SIZE(a), 4) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 5) == 3);
+      assert(search.Run(a, ARRAY_SIZE(a), 6) == 4);
+      assert(search.Run(a, ARRAY_SIZE(a), 7) == 5);
+      assert(search.Run(a, ARRAY_SIZE(a), 8) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 9) == 6);
+      assert(search.Run(a, ARRAY_SIZE(a), 10) == 7);
+      assert(search.Run(a, ARRAY_SIZE(a), 11) == -1);
+      assert(search.Run(a, ARRAY_SIZE(a), 12) == 8);
+      assert(search.Run(a, ARRAY_SIZE(a), 13) == -1);
     }
 
   }
--- Common.h
+++ Common.h
@@ -40,29 +40,6 @@ public:
 
 template<typename T> class NormalPartitioner {
 public:
-//  int operator()(T num[], int left, int right, T pivot_value) {
-//    int i = left;
-//    int j = right;
-//    while (i <= j) {
-//      while (i <= right && num[i] < pivot_value) {
-//        ++i;
-//      }
-//
-//      while (j >= left && num[j] > pivot_value) {
-//        --j;
-//      }
-//
-//      if (j <= i) {
-//        return i;
-//      }
-//
-//      std::swap(num[i], num[j]);
-//      ++i;
-//      --j;
-//    }
-//
-//    return i;
-//  }
 
   template<typename Condition> int operator()(T num[], int left, int right, Condition condition) {
     int i = left;
@@ -88,41 +65,6 @@ public:
     return i;
   }
 };
-
-//template<typename T> class OddEventPartitioner {
-//public:
-//  OddEventPartitioner() {
-//    ;
-//  }
-//
-//  bool IsOdd(T value){
-//    return (value & 1) != 0;
-//  }
-//
-//  int operator()(T num[], int left, int right) {
-//    int i = left;
-//    int j = right;
-//    while (i <= j) {
-//      while (i <= right && IsOdd(num[i])) {
-//        ++i;
-//      }
-//
-//      while (j >= left && !IsOdd(num[i])) {
-//        --j;
-//      }
-//
-//      if (j <= i) {
-//        return i;
-//      }
-//
-//      std::swap(num[i], num[j]);
-//      ++i;
-//      --j;
-//    }
-//
-//    return i;
-//  }
-//};
 
 class TestCase {
 public:
