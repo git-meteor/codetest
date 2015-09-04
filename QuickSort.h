611971d864647847e935015a5b9b9c3f3b4407c2
git-meteor <mailtohuangxin@gmail.com>
2015-09-04 13:53:43 +0800

    add HandWriteQuickSort and HandWriteQuickSort2

--- QuickSort.h
+++ QuickSort.h
@@ -32,4 +32,63 @@ public:
   }
 };
 
+class HandWriteQuickSort {
+public:
+  void Run(int a[], int n) {
+    Sort(a, 0, n - 1);
+  }
+  void Sort(int a[], int left, int right) {
+    if(left < right) {
+      int pivot_index = GetPivotIndex(left, right);
+      int partition_index = Partition(a, left, right, pivot_index);
+      Sort(a, left, partition_index - 1);
+      Sort(a, partition_index + 1, right);
+    }
+  }
+
+  int GetPivotIndex(int left, int right){
+    return (left + right) / 2;
+  }
+
+  int Partition(int a[], int left, int right, int pivot_index) {
+    std::swap(a[pivot_index], a[right]);
+    int i = left;
+    int j = left;
+    while(j < right){
+      if(a[j] < a[right]){
+        std::swap(a[i], a[j]);
+        ++i;
+      }
+      ++j;
+    }
+    std::swap(a[i], a[right]);
+    return i;
+  }
+};
+
+class HandWriteQuickSort2 {
+public:
+  void Run(int a[], int n) {
+    Sort(a, 0, n - 1);
+    std::cout << "HandWriteQuickSort2" << std::endl;
+  }
+  void Sort(int a[], int left, int right) {
+    if(left < right) {
+      int pivot_index = (left + right) / 2;
+      std::swap(a[pivot_index], a[right]);
+      int i = left, j = left;
+      while(j < right) {
+        if(a[j] < a[right]) {
+          std::swap(a[i], a[j]);
+          ++i;
+        }
+        ++j;
+      }
+      std::swap(a[i], a[right]);
+      Sort(a, left, i-1);
+      Sort(a, i+1, right);
+    }
+  }
+};
+
 #endif
--- main.cc
+++ main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cmath>
+#include <string>
 
 #include "Common.h"
 #include "BinarySearch.h"
@@ -12,11 +14,52 @@
 #include "MergeSort.h"
 
 #include "SearchRotateArray.h"
+#include "MultiplyStrings.h"
+
+int bst(int n){
+  if(n <= 0){
+      return 0;
+  }
+  std::vector<int> sum(n);
+  std::vector<int> value(n);
+  value[1] = 1;
+  sum[1] = 1;
+
+  for(int i = 2; i <= n; ++i){
+      value[i] = value[i - 1] + sum[i - 1];
+      sum[i] = sum[i - 1] + value[i];
+      std::cout << value[i] << ", " << sum[i] << std::endl;
+  }
+  return value[n];
+}
 
 int main(int argc, char* argv[]) {
+//  Solution s;
+//  std::string str1 = "0";
+//  std::string str2 = "0";
+////  s.multiply(str1, str2);
+//  std::cout << "result: " << s.multiply(str1, str2) << std::endl;
+//  return 0;
+//
+//  int m = 2;
+//  int n = 3;
+//
+//  int *b = new int[m];
+//
+//
+////  int (*a)[n] = new int[m][n];
+////  a[0][0] = 1;
+////
+////  delete []a;
+//
+//  return 0;
+//  bst(6);
+//  return 0;
+
+
 
   TestCase* test_case_array[] = {
-			new BinarySearchTest,
+//			new BinarySearchTest,
 //			new QuickSortTest,
 //      new SmallestKNumbersTest,
 //      new SelectKthTest,
@@ -24,7 +67,9 @@ int main(int argc, char* argv[]) {
 //			new PartitionTest,
 //      new InsertionSortTest,
 //      new SortTest<QuickSort<int, Median3Selector<int>, NormalPartitioner2<int> > >,
-//      new SortTest<InsertionSort<int> >,
+      new SortTest<HandWriteQuickSort2>,
+
+      //      new SortTest<InsertionSort<int> >,
 //      new SortTest<ShellSort<int> >,
 //      new SortTest<MergeSort<int> >,
   };
